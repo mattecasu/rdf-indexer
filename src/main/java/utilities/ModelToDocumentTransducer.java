64c4b61b3e8a1e337f6f75cf3e6ce415232e712e
mattecasu <mattecasu@gmail.com>
2017-11-19 16:59:05 +0000

    fixed langtag recognition

--- src/main/java/utilities/ModelToDocumentTransducer.java
+++ src/main/java/utilities/ModelToDocumentTransducer.java
@@ -1,21 +1,25 @@
 package utilities;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.util.Literals;
-import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.model.util.Statements;
 
 import java.util.Optional;
 
 import static java.util.Optional.empty;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
+@Slf4j
 public class ModelToDocumentTransducer {
 
     public static Optional<Document> translate(Model model, String uri, String endpoint) {
@@ -34,17 +38,19 @@ public class ModelToDocumentTransducer {
                 .filter(vf.createIRI(uri), null, null)
                 .stream()
                 .forEach(st -> {
-                    String prop = st.getPredicate().stringValue();
+
+                    String prop = st.getPredicate().getLocalName();
                     Value obj = st.getObject();
+
                     Optional<String> langPart = empty();
-                    if (Literals.canCreateLiteral(obj) && ((Literal) obj).getDatatype().equals(RDF.LANGSTRING)) {
-                        String lang = ((Literal) obj).getLanguage().orElse("");
+                    if (obj instanceof Literal && Literals.isLanguageLiteral((Literal) obj)) {
+                        String lang = ((Literal) obj).getLanguage().get();
                         langPart = lang.isEmpty() ? Optional.of("_xx") : Optional.of("_" + lang);
                     }
 
                     doc.add(
-                            new Field(vf.createIRI(prop).getLocalName() + langPart.orElse(""), obj.stringValue(),
-                                    TextField.TYPE_STORED));
+                            new Field(prop + langPart.orElse(EMPTY), obj.stringValue(), TextField.TYPE_STORED)
+                    );
                 });
 
         return Optional.of(doc);
