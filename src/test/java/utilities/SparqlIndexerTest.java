e5e37f91dee65a32d298f48c1a1627df6b09c585
mattecasu <mattecasu@gmail.com>
2017-11-19 16:27:52 +0000

    system home

--- src/test/java/utilities/SparqlIndexerTest.java
+++ src/test/java/utilities/SparqlIndexerTest.java
@@ -1,19 +1,21 @@
 package utilities;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-
+import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 import utilities.lucene.SparqlIndexer;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@Slf4j
 public class SparqlIndexerTest {
 
     private SparqlIndexer sesameIndexer;
     private static final String endpoint = "http://dbpedia.org/sparql";
-    private static final String sparqlIndexerPathSesame = "/Users/epi/Desktop/sesameSparqlIndexerIndex";
+    private static final String sparqlIndexerPathSesame = System.getProperty("user.home") + "/Desktop/sparqlIndexerIndex";
     private QueryConfig queryInstance;
 
     @Before
